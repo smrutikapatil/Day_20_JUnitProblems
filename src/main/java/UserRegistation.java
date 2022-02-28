266f341e78bb175af4aaaea1041e3d034aabfb2b
smrutikapatil <smruti.patil17@gmail.com>
2022-02-28 20:20:23 +0530

    Added- Checked Last name

--- src/main/java/UserRegistation.java
+++ src/main/java/UserRegistation.java
@@ -8,4 +8,10 @@ public class UserRegistation {
         Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
     }
+    public boolean lastName(String lastName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
 }
