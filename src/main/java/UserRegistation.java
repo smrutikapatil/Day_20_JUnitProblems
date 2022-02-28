6d6599947d9c9b15e85bafcb9c48b085029b63fc
smrutikapatil <smruti.patil17@gmail.com>
2022-02-28 20:10:45 +0530

    first commit

---
+++ src/main/java/UserRegistation.java
@@ -0,0 +1,8 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistation {
+        public static void main(String[] args){
+            System.out.println("Welcome message");
+        }
+    }
