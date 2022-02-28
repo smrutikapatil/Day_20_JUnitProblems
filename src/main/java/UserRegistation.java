3f305353dbbc80e21419f53e1f57a1d89c19108c
smrutikapatil <smruti.patil17@gmail.com>
2022-02-28 20:16:33 +0530

    Added- Checked first name

--- src/main/java/UserRegistation.java
+++ src/main/java/UserRegistation.java
@@ -2,7 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistation {
-        public static void main(String[] args){
-            System.out.println("Welcome message");
-        }
+    public boolean firstName(String firstName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
     }
+}
