8efd44ee6f741499b0cf36371a7958b6c8b19fef
smrutikapatil <smruti.patil17@gmail.com>
2022-02-28 20:56:20 +0530

    Added- Checked password should have at least one numeric number

--- src/main/java/UserRegistation.java
+++ src/main/java/UserRegistation.java
@@ -40,4 +40,11 @@ public class UserRegistation {
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
+
+    public boolean passwordRule3(String password) {
+        String regex = "^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9]).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
 }
