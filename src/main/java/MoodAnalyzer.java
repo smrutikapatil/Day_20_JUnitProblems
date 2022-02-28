a351c4ac0ee94ac4e9cd271697114ffa97c8e762
smrutikapatil <smruti.patil17@gmail.com>
2022-02-28 21:23:46 +0530

    Added- Added:Programme to Validating User need to Entry based on mood

---
+++ src/main/java/MoodAnalyzer.java
@@ -0,0 +1,10 @@
+public class MoodAnalyzer {
+        public static String analyseMood(String mood) {
+            if (mood.toLowerCase().contains("happy")) {
+                return "Entry Successful";
+            } else if (mood.toLowerCase().contains("sad")) {
+                return "Entry Failed";
+            } else
+                return null;
+        }
+}
