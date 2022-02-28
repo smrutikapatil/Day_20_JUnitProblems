359e62834199772a0be35bbd067d75ddeb1f0e57
smrutikapatil <96045905+smrutikapatil@users.noreply.github.com>
2022-02-28 21:08:21 +0530

    Added- Cheked password minimum 8 char and Atleast one special character

--- src/main/java/UserRegistation.java
+++ src/main/java/UserRegistation.java
@@ -8,18 +8,21 @@ public class UserRegistation {
         Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
     }
+    
     public boolean lastName(String lastName) {
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(lastName);
         return matcher.matches();
     }
+    
     public boolean email(String email) {
         String regex = "[a-z]+[.][a-z]*[@][a-z]+[.][a-z]+[.][a-z]*";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
+    
     public boolean phoneNumber(String phoneNumber) {
         String regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
         Pattern pattern = Pattern.compile(regex);
@@ -47,4 +50,11 @@ public class UserRegistation {
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
+    
+    public boolean passwordRule4(String password) {
+		String regex = "^[0-9a-zA-Z!,@#$&*().]{8,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+	}
 }
