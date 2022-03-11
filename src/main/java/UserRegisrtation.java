18fc42882f670879c8a31abe67f0c7010f7cc972
smrutikapatil <smruti.patil17@gmail.com>
2022-03-11 21:27:16 +0530

    Added- ability to parameterized test

---
+++ src/main/java/UserRegisrtation.java
@@ -0,0 +1,65 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegisrtation {
+        public boolean firstName(String firstName) {
+            String regex = "^[A-Z]{1}[a-z]{2,}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(firstName);
+            return matcher.matches();
+        }
+        public boolean lastName(String lastName) {
+            String regex = "^[A-Z]{1}[a-z]{2,}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(lastName);
+            return matcher.matches();
+        }
+        public boolean email(String email) {
+            String regex = "[a-z]+[.][a-z]*[@][a-z]+[.][a-z]+[.][a-z]*";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(email);
+            return matcher.matches();
+        }
+        public boolean phoneNumber(String phoneNumber) {
+            String regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(phoneNumber);
+            return matcher.matches();
+        }
+
+        public boolean passwordRule1(String password) {
+            String regex = "^[0-9a-zA-Z!,@#$&*().]{8,}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(password);
+            return matcher.matches();
+        }
+
+        public boolean passwordRule2(String password) {
+            String regex = "^(?=.*[A-Z]){1}(?=.*[a-z]).{8,}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(password);
+            return matcher.matches();
+        }
+
+        public boolean passwordRule3(String password) {
+            String regex = "^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9]).{8,}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(password);
+            return matcher.matches();
+        }
+
+        public boolean passwordRule4(String password) {
+            String regex = "^[0-9a-zA-Z!,@#$&*().]{8,}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(password);
+            return matcher.matches();
+        }
+
+        public boolean emailIdValidator(String emailId) {
+            String regex = "^[a-z]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+([.,]?[a-z]{2,3}){0,1}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(emailId);
+            return matcher.matches();
+        }
+    }
+
